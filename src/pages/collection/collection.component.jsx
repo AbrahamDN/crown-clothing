3d54df0913f4cdf0e24550f763ff921b3f948dd5
AbrahamDN <abe.ndinga@gmail.com>
2020-07-12 22:11:20 +0100

    + created respective selectors & updated corresponding components  with new redux flow for directory/shop

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -8,12 +8,24 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => (
-  <div className='collection-page'>
-    <h2>Collection page</h2>
-    {console.log(collection)}
-  </div>
-);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+
+  return (
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map(item => (
+          <CollectionItem
+            className='collection-item'
+            key={item.id}
+            item={item}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state),
