cabe1443ac6491ad93f04347132d55d442075f61
AbrahamDN <abe.ndinga@gmail.com>
2020-07-16 03:20:12 +0100

    + add a async function that lets us add collections and documents to the firebase database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -17,8 +17,10 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
   const userRef = firestore.doc(`/users/${userAuth.uid}`);
+  const collectionRef = firestore.collection('users');
 
   const snapShot = await userRef.get();
+  const collectionSnapchot = await collectionRef.get();
 
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
@@ -37,6 +39,21 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
 
   return userRef;
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
 };
 
 firebase.initializeApp(config);
